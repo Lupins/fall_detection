79f15bf70722c154cf3bed5850785ce57d0a5a7d
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2019-05-02 23:46:51 +0000

    docker to include mnt

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,1 @@
-sudo nvidia-docker run --rm --runtime=nvidia --name leite -it -v "/home/leite/workspace:/workspace" "semantix/v0.0.1" /bin/bash
+sudo nvidia-docker run --rm --runtime=nvidia --name leite -it -v "/mnt:/mnt" -v "/home/leite/workspace:/workspace" "semantix/v0.0.1" /bin/bash
