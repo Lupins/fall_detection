9a83c4face8804fedcfbec1b4fd892244e8012f7
root <guilherme.vieira.leite@gmail.com>
2019-05-07 12:58:05 +0000

    0957

--- saliency_extractor/shell.sh
+++ saliency_extractor/shell.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+<<<<<<< HEAD
 
 for f in input/*
 do
   python frame_int_grad_saliency.py $f
+=======
+#for f in input/*
+#do
+  #python int_grad_saliency.py $f | cut -f2 -d"/"
+#done
+
+#python int_grad_saliency.py fall-07-cam0.mp4
+
+for f in input/*
+do
+  python int_grad_saliency.py $f
+>>>>>>> 66c7e669d4f5d6235c5f6b6a32ee5f1cfdabfebf
 done
