24df78cde7272058cd85e66b84ba62075a6e05fe
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2019-05-07 13:20:36 +0000

    gabriel new

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,1 @@
-sudo nvidia-docker run --rm --name leite --runtime=nvidia -it -u docker -v "/home/leite/workspace/:/home/docker/workspace/" "leite:fall" /bin/bash
+sudo nvidia-docker run --rm --runtime=nvidia --name leite -it -v "/mnt:/mnt" -v "/home/leite/workspace:/workspace" "semantix/v0.0.1" /bin/bash
