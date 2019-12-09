107c0386915786ee07094e769f099af5b62d03c1
leite-dell <guilherme.vieira.leite@gmail.com>
2019-12-09 17:25:26 -0300

    deleted cached files

--- 3d_conv/run.sh
+++
@@ -1,1 +0,0 @@
-python npy_creator.py -dataset /mnt/Data/leite/URFD/cp-train/ -classes Falls NotFalls -streams saliency pose frame ritmo flow_z
--- docker/run.sh
+++
@@ -1,1 +0,0 @@
-sudo nvidia-docker run --rm --runtime=nvidia --name leite -it -v "/mnt:/mnt" -v "/home/leite/workspace:/workspace" "semantix/v0.0.1" /bin/bash
